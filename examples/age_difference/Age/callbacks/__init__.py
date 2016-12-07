95a06b572bdafd2041e7de07d72fe168996a4a8b
Brian Zambrano <brianz@gmail.com>
2016-12-07 13:08:23 -0700

    Create an example app to calculate age
    Why
    ---
    
    - Shows how to use dynamic responses
    
    This change addresses the need by
    ---------------------------------
    
    - Creating a simple app to get the date from a user and then returning a
      dynamic response based on the persons age.
    
    Next Steps
    ----------
    
    - None

--- examples/age_difference/Age/callbacks/__init__.py
+++ examples/age_difference/Age/callbacks/__init__.py
@@ -1,1 +1,44 @@
-from calc_difference import age_difference
+import random
+
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+from lazysusan.logger import get_logger
+from lazysusan.response import build_response_payload
+
+
+def calc_difference(request, session, intent, context, user_id, state_machine):
+    log = get_logger()
+    # get the date
+    try:
+        date = request["intent"]["slots"]["dob"]["value"]
+        log.info("Got date from slots: %s" % (date, ))
+
+        now = datetime.now()
+        dob = datetime.strptime(date, "%Y-%m-%d")
+        age = relativedelta(now, dob)
+
+        if not age.months and not age.days:
+            msg = "Happy birthday! You are %s years old today" % (age.years, )
+        else:
+            msg = "You are %s years, " % (age.years, )
+            if age.months > 0:
+                if age.months == 1:
+                    msg += "one month, "
+                else:
+                    msg += "%s months, " % (age.months, )
+
+            if age.days > 0:
+                if age.days == 1:
+                    msg += "one day old"
+                else:
+                    msg += "%s days old" % (age.months, )
+
+        response = state_machine["ageResponse"]["response"]
+        response["outputSpeech"]["text"] = msg
+        return build_response_payload(response, session.get_state_params())
+    except KeyError:
+        date = ""
+        log.error("Could  not find date in slots")
+
+    return "goodBye"
--- examples/age_difference/Age/callbacks/age_difference.py
+++
@@ -1,5 +0,0 @@
-import random
-
-
-def calc_difference(request, state_machine):
-    return 0
